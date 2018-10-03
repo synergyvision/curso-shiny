cc3027b63912796a7f9f5203486a0a0fd28a46c2
Alexander A. Ramírez M <alexander.ramirez@synergy-gb.com>
2018-10-02 22:02:22 -0400

    Cambio en las instrucciones

--- practica/001/Readme.md
+++ practica/001/Readme.md
@@ -4,6 +4,12 @@ Para entender lo que ocurre vamos a ejecutar los comandos siguientes en la cóns
 
 - **fluidPage()**
 - **fluidPage(titlePanel("Hola Synergy Vision"))**
+- **titlePanel("Hello Synergy Vision!")**
+- **titlePanel(div("Hola Synergy Vision!", style = "font-family: 'Lobster', cursive; font-weight: 500; line-height: 1.1; color: #acacac;"))**
+- **sidebarLayout()**
+- **sidebarLayout(sidebarPanel())**
+- **sidebarLayout(sidebarPanel(),mainPanel())**
+- Ver la ayuda de **sliderInput** y **plotOutput**
 
 En este ejemplo básico vamos a crear un control **slider**
 
--- practica/001/app.R
+++ practica/001/app.R
@@ -4,36 +4,30 @@ library(shiny)
 # y un Histograma
 #
 ui <- fluidPage(
-  # Título
-  titlePanel("Hello Synergy Vision!"),
+  # Generación de un título
+  titlePanel("Hola Synergy Vision!",
+             windowTitle = "Synergy Vision"),
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-
     # Sidebar panel for inputs ----
     sidebarPanel(
-
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
                   min = 1,
                   max = 50,
                   value = 30)
-
     ),
-
     # Main panel for displaying outputs ----
     mainPanel(
-
       # Output: Histogram ----
       plotOutput(outputId = "distPlot")
-
     )
   )
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-
   # Histogram of the Old Faithful Geyser Data ----
   # with requested number of bins
   # This expression that generates a histogram is wrapped in a call
@@ -43,16 +37,12 @@ server <- function(input, output) {
   #    re-executed when inputs (input$bins) change
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
-
     x    <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
     hist(x, breaks = bins, col = "#75AADB", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
-
     })
-
 }
 
 # Create Shiny app ----
